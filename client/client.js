e940da6705f50cf7942f028cfc50034f7f8463a7
Manuel Germán Morales <mgm00193@red.ujaen.es>
2021-03-11 23:42:38 +0100

    Added TURN servers. Linked adapter.js with index.html

--- client/client.js
+++ client/client.js
@@ -7,8 +7,23 @@ var llamandoA = "" //ID del usuario con el que estoy en llamada
 /**
  * PEERCONNECTION CONFIG
  */
-const config = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}, 
-                               {'urls': 'stun:stun4.l.google.com:19302'}]}
+const config = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}, 
+                               {'url': 'stun:stun4.l.google.com:19302'},
+                               {
+                                'url': 'turn:numb.viagenie.ca',
+                                'credential': 'muazkh',
+                                'username': 'webrtc@live.com'
+                                },
+                                {
+                                'url': 'turn:192.158.29.39:3478?transport=udp',
+                                'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                                'username': '28224511:1379330808'
+                                },
+                                {
+                                'url': 'turn:192.158.29.39:3478?transport=tcp',
+                                'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                                'username': '28224511:1379330808'
+                              }]}
 
 /**
  * STREAM RESTRICTIONS
